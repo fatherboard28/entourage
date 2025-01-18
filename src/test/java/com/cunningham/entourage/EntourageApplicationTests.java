4f418ea24eb9c1df7ac03b8b30f807f257d4edaa
Jonathan Cunningham <jonathanscunningham@gmail.com>
2025-01-17 20:06:48 -0600

    inital commit

---
+++ src/test/java/com/cunningham/entourage/EntourageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunningham.entourage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntourageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
